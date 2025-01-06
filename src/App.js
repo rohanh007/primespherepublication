4acfe794763b646458932f06441a1ea6900c047c
rohanh007 <rohanhoval007@gmail.com>
2025-01-06 14:59:18 +0530

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,52 +1,47 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import Home from './Components/Home/Home';
-import Services from './Components/Services/Services';
-import Aboutus from './Components/Aboutus/Aboutus';
-import { BrowserRouter as Router, Routes,Route, BrowserRouter } from 'react-router-dom';
-// import Publications from './Components/Publications/Publications';
-import ServicesPage from './Components/Services/Services.jsx';
-import ContactUs from './Components/Contactus/Contactus.jsx';
-import Scrolltop from './Components/Scrolltop/Scrolltop.jsx';
-import Footer from './Components/Footer/Footer.jsx';
-import CorpSocialResp from './Components/Aboutus/CorpSocialResp.jsx';
-import AboutPublications from './Components/Publications/AboutPublications.jsx';
-import JournalsList from './Components/Publications/JournalsList.jsx';
-import SubmitArticles from './Components/Publications/SubmitArticles.jsx';
-import CallForPaper from './Components/Publications/CallForPaper.jsx';
-import PrivacyPolicy from './Components/Publications/PrivacyPolicy.jsx';
-import RefundPolicy from './Components/Publications/RefundPolicy.jsx';
-import JournalDetail from './Components/Publications/JournalDetail.jsx';
-import Assistant from './Components/FixedContact/Assistant.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Lazy loading of components
+const Home = React.lazy(() => import('./Components/Home/Home'));
+const ServicesPage = React.lazy(() => import('./Components/Services/Services.jsx'));
+const Aboutus = React.lazy(() => import('./Components/Aboutus/Aboutus'));
+const CorpSocialResp = React.lazy(() => import('./Components/Aboutus/CorpSocialResp.jsx'));
+const AboutPublications = React.lazy(() => import('./Components/Publications/AboutPublications.jsx'));
+const JournalsList = React.lazy(() => import('./Components/Publications/JournalsList.jsx'));
+const CallForPaper = React.lazy(() => import('./Components/Publications/CallForPaper.jsx'));
+const PrivacyPolicy = React.lazy(() => import('./Components/Publications/PrivacyPolicy.jsx'));
+const RefundPolicy = React.lazy(() => import('./Components/Publications/RefundPolicy.jsx'));
+const ContactUs = React.lazy(() => import('./Components/Contactus/Contactus.jsx'));
+const JournalDetail = React.lazy(() => import('./Components/Publications/JournalDetail.jsx'));
+const Scrolltop = React.lazy(() => import('./Components/Scrolltop/Scrolltop.jsx'));
+const Footer = React.lazy(() => import('./Components/Footer/Footer.jsx'));
+const Assistant = React.lazy(() => import('./Components/FixedContact/Assistant.jsx'));
 
 function App() {
   return (
-    <div className="App no-scrollbar ">
-      
-      <BrowserRouter>
-      <Scrolltop/>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/services' element={<ServicesPage/>}/>
-        {/* <Route path='/Publications' element={<Publications/>}/> */}
-        <Route path='/aboutus' element={<Aboutus/>}/>
-        <Route path='/corporate-social-responsibility' element={<CorpSocialResp/>}/>
-        <Route path='/about-publication' element={<AboutPublications/>}/>
-        <Route path='/journals' element={<JournalsList/>}/>
-        {/* <Route path='/submit-articles' element={<SubmitArticles/>}/> */}
-        <Route path='/call-for-paper' element={<CallForPaper/>}/>
-        <Route path='/privacy-policy' element={<PrivacyPolicy/>}/>
-        <Route path='/refund-policy' element={<RefundPolicy/>}/>
-        <Route path='/contactus' element={<ContactUs/>}/> 
-         {/* journal details route  */}
-        <Route path="/journal/:id" element={<JournalDetail/>} />
-      </Routes>
-      <Assistant/>
-      <Footer/>
-      </BrowserRouter>
-         
+    <div className="App no-scrollbar">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <Scrolltop />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/aboutus" element={<Aboutus />} />
+            <Route path="/corporate-social-responsibility" element={<CorpSocialResp />} />
+            <Route path="/about-publication" element={<AboutPublications />} />
+            <Route path="/journals" element={<JournalsList />} />
+            <Route path="/call-for-paper" element={<CallForPaper />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/refund-policy" element={<RefundPolicy />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/journal/:id" element={<JournalDetail />} />
+          </Routes>
+          <Assistant />
+          <Footer />
+        </Router>
+      </Suspense>
     </div>
   );
 }
