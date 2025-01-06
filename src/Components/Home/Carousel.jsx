316bad7a2f0a8d27d802461f603587d39bac96b7
Rohan Hoval <77897898+rohanh007@users.noreply.github.com>
2025-01-06 13:06:25 +0000

    check

--- src/Components/Home/Carousel.jsx
+++ src/Components/Home/Carousel.jsx
@@ -1,22 +1,17 @@
 import React, { useEffect, useState } from "react";
-// import slider_1 from '../../Static/Slider/slider_1.webp';
-// import slider_2 from '../../Static/Slider/slide_2.jpg';
-// import slider_3 from '../../Static/Slider/slide_3.jpg';
-// import slider from '../../../public/Slider/slider_1.webp'
+import slider_1 from "../../assets/Slider/slider_1.webp";
+import slider_2 from "../../assets/Slider/slide_2.webp";
+import slider_3 from "../../assets/Slider/slide_3.webp";
+
 const Carousel = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  const slides = [
-    `../../public/Slider/slider_1.webp`,
-    `../../public/Slider/slide_2.webp`,
-    `../../public/Slider/slide_3.webp`,
-  ];
-  
+  const slides = [slider_1, slider_2, slider_3];
 
   useEffect(() => {
     const timer = setTimeout(() => {
       handleNext();
-    }, 3000); 
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, [currentSlide]);
@@ -33,29 +28,25 @@ const Carousel = () => {
     setCurrentSlide(index);
   };
 
-  return ( 
-    <div className="relative w-full ">
-      {/* Carousel Wrapper */}
+  return (
+    <div className="relative w-full">
       <div className="relative h-screen overflow-hidden">
         {slides.map((slide, index) => (
-          
           <div
             key={index}
             className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${
               currentSlide === index ? "opacity-100" : "opacity-0"
             }`}
           >
-            {console.log(slide)}
             <img
               src={slide}
               alt={`Slide ${index + 1}`}
-              className="block w-full h-full object-cover blur-[4px] brightness-50"
+              className="block w-full h-full object-cover"
             />
           </div>
         ))}
       </div>
 
-      {/* Indicators */}
       <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3">
         {slides.map((_, index) => (
           <button
@@ -68,52 +59,6 @@ const Carousel = () => {
           ></button>
         ))}
       </div>
-
-      {/* Controls */}
-      {/* <button
-        onClick={handlePrev}
-        className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        aria-label="Previous"
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white">
-          <svg
-            className="w-4 h-4 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 1L1 5l4 4"
-            />
-          </svg>
-        </span>
-      </button>
-      <button
-        onClick={handleNext}
-        className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        aria-label="Next"
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white">
-          <svg
-            className="w-4 h-4 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 9l4-4-4-4"
-            />
-          </svg>
-        </span>
-      </button> */}
     </div>
   );
 };
